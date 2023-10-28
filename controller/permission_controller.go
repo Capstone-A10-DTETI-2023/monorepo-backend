6cf46849f27af51894ae35c53d4981050482ee41
yosefadi <me@yosefadi.my.id>
2023-10-28 20:13:15 +0700

    patch(perm): cleanup api response

--- controller/permission_controller.go
+++ controller/permission_controller.go
@@ -11,6 +11,15 @@ type PermissionController struct {
 	DB *gorm.DB
 }
 
+type PermissionResponse struct {
+	RoleID 					uint   `json:"role_id"`
+	Read_Realtime_Data 		bool   `json:"read_realtime_data"`
+	Read_Historical_Data 	bool   `json:"read_historical_data"`
+	Change_Actuator 		bool   `json:"change_actuator"`
+	User_Management 		bool   `json:"user_management"`
+	Node_Management 		bool   `json:"node_management"`
+}
+
 func (c *PermissionController) GetAllPermission(ctx *fiber.Ctx) error {
 	if isAdmin := middleware.IsAdmin(ctx); !isAdmin {
 		return fiber.ErrUnauthorized
@@ -22,8 +31,20 @@ func (c *PermissionController) GetAllPermission(ctx *fiber.Ctx) error {
 		return err
 	}
 
+	var permResponses []PermissionResponse
+	for _, perm := range perms {
+		permResponses = append(permResponses, PermissionResponse{
+			RoleID: perm.RoleID,
+			Read_Realtime_Data: perm.Read_Realtime_Data,
+			Read_Historical_Data: perm.Read_Historical_Data,
+			Change_Actuator: perm.Change_Actuator,
+			User_Management: perm.User_Management,
+			Node_Management: perm.Node_Management,
+		})
+	}
+
 	return ctx.JSON(fiber.Map{
 		"message": "success",
-		"data":   perms,
+		"data":   permResponses,
 	})
 }
